170a9e01c05fba6118458afc0ae7052e0154f5f8
VictorAparecida <87038118+VictorAparecida@users.noreply.github.com>
2024-03-15 22:51:20 -0300

    Add files via upload

---
+++ Contador0a100.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Count(0);
+    }
+
+    static void Count(int num)
+    {
+        if (num <= 100)
+        {
+            Console.WriteLine(num);
+            Count(num + 1);
+        }
+    }
+}
---
+++ ForeachInverso.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numeros = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+
+        foreach (int num in numeros)
+        {
+            switch (num)
+            {
+                case 9:
+                    Console.Write("09");
+                    break;
+                default:
+                    Console.Write(num);
+                    break;
+            }
+        }
+        Console.WriteLine();
+    }
+}
